4c8ac9ebe852d68bf522668f353ddbf615c90466
seth mcfeeters <seth.mcfeeters@gmail.com>
2020-11-10 14:10:15 -0800

    sudo setup

---
+++ math_series/series.py
@@ -0,0 +1,21 @@
+def fibonacci(n)
+    """ 
+    adds previous value to next value. starts with zero and one
+    returns nth value
+    """
+
+def lucas(n)
+""" 
+adds previous value to next value. starts with 2 and 1. 
+returns nth value
+""" 
+
+def sum_series
+""" 
+takes in one required perameter and two optional parameters
+req perameter determines which series will be printed
+perammeters call previous functions.
+no optional arg print fibonacci
+arguments 2 and 1 print lucas
+
+"""
